c6362e0eee934d230fdc997546a6a30691598e58
jangyh07222 <jangyh07222@gmail.com>
2018-11-22 14:33:34 +0900

    181122

--- js1/public/js/wheel.js
+++ js1/public/js/wheel.js
@@ -1,37 +1,59 @@
+var pages = new WheelScroll({
+	page: ".page",
+	nav: ".nav", 
+	speed: 300
+});
+//var pages = new WheelScroll();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var scTop = $(window).scrollTop();
 var gap = [];
 var now = 0;
-$(window).resize(function () {
-    $(".page").each(function (i) {
-        gap[i] = $(this).offset().top;
-    });
+$(window).resize(function(){
+	$(".page").each(function (i) {
+		gap[i] = $(this).offset().top;
+	});
 }).trigger("resize");
 $(window).on("mousewheel DOMMouseScroll", wheelFn);
-
 function wheelFn(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    var dir = e.originalEvent.wheelDelta;
-    $(window).off("mousewheel DOMMouseScroll");
-    scTop = $(window).scrollTop();
-    for (var i = 0; i < gap.length; i++) {
-        if (scTop <= gap[i]) {
-            now = i;
-            break;
-        }
-    }
-    if (dir > 0) {
-        if (now > 0) now--;
-    } else {
-        if (now < gap.length - 1) now++;
-    }
-    $("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 700, function () {
-        $(window).on("mousewheel DOMMouseScroll", wheelFn);
-    });
+	e.preventDefault();
+	e.stopPropagation();
+	var dir = e.originalEvent.wheelDelta;
+	$(window).off("mousewheel DOMMouseScroll");
+	scTop = $(window).scrollTop();
+	for (var i=0; i<gap.length; i++) {
+		if (scTop <= gap[i]) {
+			now = i;
+			break;
+		}
+	}
+	if (dir > 0) { if (now > 0) now--; } 
+	else { if (now < gap.length - 1) now++; }
+	$("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 200, function(){
+		$(window).on("mousewheel DOMMouseScroll", wheelFn);
+	});
 }
-$(".nav").on("click", function () {
-    var oldNow = now;
-    now = $(this).data("now");
-    var speedGap = Math.abs(now - oldNow);
-    $("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 500*speedGap);
+$(".nav").on("click", function(){
+	var oldNow = now;
+	now = $(this).data("now");
+	var speedGap = Math.abs(now - oldNow);
+	$("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 100*speedGap);
 });
+*/
