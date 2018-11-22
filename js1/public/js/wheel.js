aa5d0302bbc3eeccc74c31ffc19441f116e98e20
jangyh07222 <jangyh07222@gmail.com>
2018-11-22 12:03:08 +0900

    페이지단위 스크롤 기능

---
+++ js1/public/js/wheel.js
@@ -0,0 +1,37 @@
+var scTop = $(window).scrollTop();
+var gap = [];
+var now = 0;
+$(window).resize(function () {
+    $(".page").each(function (i) {
+        gap[i] = $(this).offset().top;
+    });
+}).trigger("resize");
+$(window).on("mousewheel DOMMouseScroll", wheelFn);
+
+function wheelFn(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var dir = e.originalEvent.wheelDelta;
+    $(window).off("mousewheel DOMMouseScroll");
+    scTop = $(window).scrollTop();
+    for (var i = 0; i < gap.length; i++) {
+        if (scTop <= gap[i]) {
+            now = i;
+            break;
+        }
+    }
+    if (dir > 0) {
+        if (now > 0) now--;
+    } else {
+        if (now < gap.length - 1) now++;
+    }
+    $("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 700, function () {
+        $(window).on("mousewheel DOMMouseScroll", wheelFn);
+    });
+}
+$(".nav").on("click", function () {
+    var oldNow = now;
+    now = $(this).data("now");
+    var speedGap = Math.abs(now - oldNow);
+    $("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 500*speedGap);
+});
